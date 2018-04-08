cd14e78383e28b8d9de8c4784c7b92bd56793f67
qCuteArthur <2217594536@qq.com>
2018-04-08 18:02:26 +0800

    2018年4月8日18:02:15

---
+++ BS_CSharp/BS_LINQ/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS_LINQ
+{
+    public delegate void MyDele();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            M1 m1 = new M1();
+            MyDele myDele = new MyDele(m1.Action);
+            myDele += m1.Action;
+            Console.ReadLine();
+        }
+    }
+    public class M1
+    {
+        public void Action()
+        {
+            Console.WriteLine("+++++++++++++++");
+        }
+    }
+}
+//大纲+++++++++++++++++++++++++++++++++++++++++++
+//委托一个是可以用事件，一个是可以用Lambda表达式。
+
+//    委托是啥
+//    委托声明
+//    泛型委托
+//    必须自己创建委托类型吗
+//    泛型委托类型参数推断
+
+//    方法和lambda之间的关系
+//    把一个lambda表达式赋值给委托类型变量
+//    把一个Lambda给一个委托类型的参数
+
+//LINQ
+
+//细节开始++++++++++++++++++++++++++++++++++++
+//委托，其实是委托类型，是一种class类型。
+//把引用类型声明的变量，声明给实例
+
+//委托类型声明一个变量之后，可以创建一个委托实例或者直接给null
+
+//功能特殊，看起来比较特殊。
+//一般的类都是反映现实生活中的事物，委托类型是用来包裹方法，通过委托类型的实例来间接的调用方法，委托类型就是方法的实例。
+//委托就是用来间接调用。
+
+
+//委托的声明类似于一个函数的签名。
+//delegate void MyDele();
+//就是你使用的时候，需要进行实例化，也就是创建实际引用的变量。
+//这种委托类型的参数，只要返回值是void，参数列表是空就行，不管你是实例方法还是静态方法，统统能应用。
+
+//未来对于委托类型的变量的使用
+
--- BS_CSharp/BS_事件，简略声明/Program.cs
+++ BS_CSharp/BS_事件，简略声明/Program.cs
@@ -8,10 +8,84 @@ using System.Threading.Tasks;
 //拒绝使用简略声明，反正事件简化声明根本让人看不懂。
 namespace BS_事件_简略声明
 {
+    public delegate void ActionEventHandler(Customer customer, ActioneEventArgs e);
+
+    public class ActioneEventArgs
+    {
+        public string DishName { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Customer custumer = new Customer();
+            Waiter waiter = new Waiter();
+            custumer.Action += waiter.Action;
+            custumer.OnAction();
+            Console.ReadLine();
         }
     }
+
+    public class Waiter
+    {
+        internal void Action(Customer customer, ActioneEventArgs e)
+        {
+            Console.WriteLine("---------------");
+        }
+    }
+
+    public class Customer
+    {
+        public ActionEventHandler actionEventHandler;
+        //事件要有自己对应的事件处理器
+        public event ActionEventHandler Action{
+            add {
+                actionEventHandler += value;
+            }
+            remove
+            {
+                actionEventHandler -= value;
+            }
+        }
+        //专门触发事件的方法，定义为OnEvent();
+        public void OnAction()
+        {
+            //必须判断这个委托是不是为空。
+            if(this.actionEventHandler != null)
+            {
+                Console.WriteLine("++++++++++++++");
+                ActioneEventArgs e = new ActioneEventArgs() { DishName = "Chicken" };
+                this.actionEventHandler.Invoke(this, e);
+            }
+        }
+        //为什么不应该是public，而是继承类的protected。
+        //应该说，就是防止外界触发这个事件，这个事件的拥有者才会触发这个事件，继承类，而不是一个命名空间。
+    }
 }
+//event的本质就是委托字段的一个包装器，对于委托字段的访问起到了限制作用。事件对于外界隐藏了大多数的功能，仅仅暴露了添加和移除时间处理器的功能。
+
+//用于声明事件的委托类型的命名约定
+//一般就是FooEventHandler（object sender ，EventArgs e）
+//触发时间的方法一般叫做On-event
+
+//事件——限制了外界对代码的访问
+
+//委托中的借刀杀人，访问级别要定义为protected而不是public。
+//一个方法做了两件事情，就是借刀杀人。
+//对于触发事件的方法，需要使用protected进行声明
+
+//存储数据 做事情 通知别人
+//例如：Form closing事件，可以实现对于是不是真的要关闭的确认。
+//可以增加log进行记录。
+
+//关于事件的误会，事件是一种以特殊方式声明的委托字段——主要是field-like简化声明的字段，坑人啊。所以，现在还是认真写全版。
+//事件是不能比较是不是空的，而是事件处理器是不是为空。
+//事件不是委托字段本身
+
+    //用委托声明事件——source角度：表明source对外可以传递什么消息
+    //在订阅者角度看、被通知者——是一种约定，约定了subscribe者可以处理的消息。
+    //委托类型的实例来存储或者引用事件处理器。
+
+    //对比事件和属性——字段包装器，很简单的保护字段。
+    //事件——委托类型字段的包装器，保证这个EventHandler不要被外界随意调用，起码要满足object和eventArgs。防止被借刀杀人。
