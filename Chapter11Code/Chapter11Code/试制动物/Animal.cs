43b5153f6f863441617bdb8542c6b16814852b3a
qCuteArthur <2217594536@qq.com>
2018-04-13 00:25:04 +0800

    C# 入门教程

---
+++ Chapter11Code/Chapter11Code/试制动物/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 试制动物
+{
+    public abstract class Animal
+    {
+        protected string name;
+        //可以从
+        public string Name
+        {
+            get { return name;}
+            set { name = value; }
+        }
+        public Animal()
+        {
+            name = "this animal has no name.";
+        }
+        public Animal(string newName)
+        {
+            name = newName;
+        }
+        public void Feed()
+        {
+            Console.WriteLine("{0} has been fed.",name);
+        }
+    }
+}
---
+++ leetcode_white/tsts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tsts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(Animal myAnimal in animalArrayList)
+            {
+                Console.WriteLine("New {0} object added to ArrayList collection,"+  "Name = {1}",myAnimal.ToString(),myAnimal.Name);
+            }
+        }
+    }
+}
